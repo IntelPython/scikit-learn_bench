a17d2aeafe27c4c24bd6ea1a7bce884573d4b792
Sameer Bibikar <sameer.bibikar@intel.com>
2019-06-10 17:24:56 -0500

    Add extern "C" where needed

--- native/lbfgsb/lbfgsb.h
+++ native/lbfgsb/lbfgsb.h
@@ -12,9 +12,15 @@
 #ifndef __LBFGSB_H__
 #define __LBFGSB_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void setulb_(int *n, int *m, double *x, double *l, double *u, int *nbd,
             double *f, double *g, double *factr, double *pgtol, double *wa,
             int *iwa, char *task, int *iprint, char *csave, int *lsave,
             int *isave, double *dsave, long int task_len, long int csave_len);
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __LBFGSB_H__ */
