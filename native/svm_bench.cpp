f4e043b0b7a7d9b51e4f5662f9ba5d9fef62a160
Sameer Bibikar <sameer.bibikar@intel.com>
2019-07-17 16:19:32 -0500

    Remove common_svm.hpp and two_class_svm_bench.cpp

--- native/svm_bench.cpp
+++ native/svm_bench.cpp
@@ -17,13 +17,33 @@
 #include "daal.h"
 #include "CLI11.hpp"
 #include "common.hpp"
-#include "common_svm.hpp"
 #include "npyfile.h"
 
 namespace dam = daal::algorithms::multi_class_classifier;
 
-using namespace daal;
-using namespace da;
+
+struct svm_params {
+    double C;
+    double tol;
+    double tau;
+    int max_iter;
+};
+
+
+void print_svm_params(svm_params p) {
+    std::clog << "@ { C: " << p.C
+        << ", tol: " << p.tol
+        << ", tau: " << p.tau
+        << ", max_iter: " << p.max_iter
+        << "}" << std::endl;
+}
+
+
+size_t get_optimal_cache_size(size_t n) {
+    return sizeof(double) * n * n;
+}
+
+
 std::vector<int >
 lexicographic_permutation(int n_cl)
 {
@@ -61,17 +81,17 @@ group_indices_by_class(int n_classes, double *labels, std::vector<std::vector<in
     std::vector<std::vector<int> > sv_ind_by_class;
     sv_ind_by_class.resize(n_classes);
     for (std::vector<std::vector<int> >::iterator it = sv_ind_by_clf.begin(); it != sv_ind_by_clf.end(); ++it) {
-	std::vector<int> v = *it;
-	for(std::vector<int>::iterator it2 = v.begin(); it2 != v.end() ; ++it2) {
-	    int idx = *it2;
-	    int lbl = static_cast<int>( ROUND(labels[idx]) );
+    std::vector<int> v = *it;
+    for(std::vector<int>::iterator it2 = v.begin(); it2 != v.end() ; ++it2) {
+        int idx = *it2;
+        int lbl = static_cast<int>( ROUND(labels[idx]) );
             sv_ind_by_class[lbl].push_back(idx);
 
             if (lbl > max_lbl) max_lbl = lbl;
-	}
+    }
     }
     if (max_lbl + 1 < n_classes) {
-	sv_ind_by_class.resize(max_lbl+1);
+    sv_ind_by_class.resize(max_lbl+1);
     }
 
     return sv_ind_by_class;
@@ -85,13 +105,13 @@ map_sv_to_columns_on_dual_coef_matrix(std::vector<std::vector<int> > sv_ind_by_c
     std::map<int, int> sv_ind_mapping;
     int p = 0;
     for(auto indices_per_class : sv_ind_by_class) {
-	std::sort(indices_per_class.begin(), indices_per_class.end());
+    std::sort(indices_per_class.begin(), indices_per_class.end());
         for(auto sv_index : indices_per_class) {
-	    if (! IS_IN_MAP(sv_ind_mapping, sv_index)) {
-		sv_ind_mapping[sv_index] = p;
-		++p;
-	    }
-	}
+        if (! IS_IN_MAP(sv_ind_mapping, sv_index)) {
+        sv_ind_mapping[sv_index] = p;
+        ++p;
+        }
+    }
     }
 
     return sv_ind_mapping;
@@ -103,13 +123,13 @@ permute_vector(std::vector<T> &v, const std::vector<int> &perm)
     std::vector<T> v_permuted;
     v_permuted.reserve(v.size());
     for (auto idx : perm)
-	v_permuted.push_back(v[idx]);
+    v_permuted.push_back(v[idx]);
     std::swap(v, v_permuted);
 }
 
 size_t
 construct_dual_coefs(
-    multi_class_classifier::training::ResultPtr training_result,
+    dam::training::ResultPtr training_result,
     int n_classes,
     dm::NumericTablePtr Y_nt,
     int n_rows,
@@ -120,7 +140,7 @@ construct_dual_coefs(
     Y_nt->getBlockOfRows(0, n_rows, dm::readOnly, blockY);
     double *Y_data_ptr = blockY.getBlockPtr();
 
-    dam::ModelPtr multi_svm_model = training_result->get(classifier::training::model);
+    dam::ModelPtr multi_svm_model = training_result->get(da::classifier::training::model);
 
     int num_models = multi_svm_model->getNumberOfTwoClassClassifierModels();
 
@@ -131,47 +151,47 @@ construct_dual_coefs(
     std::vector<std::vector<int> > label_indexes(n_classes);
 
     for(int i1=0, model_id=0; i1 < n_classes; i1++) {
-	for(int j=0; j < n_rows; j++)
-	    if (Y_data_ptr[j] == i1) label_indexes[i1].push_back(j);
+    for(int j=0; j < n_rows; j++)
+        if (Y_data_ptr[j] == i1) label_indexes[i1].push_back(j);
 
         int idx_len = label_indexes[i1].size();
 
-	for(int i2=0; i2 < i1; i2++, model_id++) {
-	    auto classifier_model = multi_svm_model->getTwoClassClassifierModel(model_id);
-	    svm::ModelPtr bin_svm_model = ds::dynamicPointerCast<svm::Model>(classifier_model);
+    for(int i2=0; i2 < i1; i2++, model_id++) {
+        auto classifier_model = multi_svm_model->getTwoClassClassifierModel(model_id);
+        da::svm::ModelPtr bin_svm_model = ds::dynamicPointerCast<da::svm::Model>(classifier_model);
 
-	    dm::NumericTablePtr sv_indx = bin_svm_model->getSupportIndices();
+        dm::NumericTablePtr sv_indx = bin_svm_model->getSupportIndices();
 
             /* sv_ind = np.take(np.hstack((label_indexes[i1], label_indexes[i2])), two_class_sv_ind_.ravel())
                sv_ind_by_clf.append(sv_ind) */
 
-	    dm::BlockDescriptor<int> block_sv_indx;
+        dm::BlockDescriptor<int> block_sv_indx;
             int two_class_classifier_sv_len = sv_indx->getNumberOfRows();
             sv_indx->getBlockOfRows(0, two_class_classifier_sv_len, dm::readOnly, block_sv_indx);
-	    int *sv_ind_data_ptr = block_sv_indx.getBlockPtr();
+        int *sv_ind_data_ptr = block_sv_indx.getBlockPtr();
 
-	    for (int j=0; j < two_class_classifier_sv_len; j++) {
+        for (int j=0; j < two_class_classifier_sv_len; j++) {
                 int sv_idx = sv_ind_data_ptr[j];
                 sv_ind_by_clf[model_id].push_back(
-		    (sv_idx < idx_len) ?
-		        label_indexes[i1][sv_idx          ] :
-		        label_indexes[i2][sv_idx - idx_len] );
-	    }
+            (sv_idx < idx_len) ?
+                label_indexes[i1][sv_idx          ] :
+                label_indexes[i2][sv_idx - idx_len] );
+        }
             sv_indx->releaseBlockOfRows(block_sv_indx); 
  
-	    auto bias = bin_svm_model->getBias();
-	    intercepts.push_back(-bias);
+        auto bias = bin_svm_model->getBias();
+        intercepts.push_back(-bias);
 
-	    auto sv_coefs = bin_svm_model->getClassificationCoefficients();
-	    dm::BlockDescriptor<double> block_sv_coefs;
-	    sv_coefs->getBlockOfRows(0, two_class_classifier_sv_len, dm::readOnly, block_sv_coefs);
+        auto sv_coefs = bin_svm_model->getClassificationCoefficients();
+        dm::BlockDescriptor<double> block_sv_coefs;
+        sv_coefs->getBlockOfRows(0, two_class_classifier_sv_len, dm::readOnly, block_sv_coefs);
             double *sv_coefs_ptr = block_sv_coefs.getBlockPtr();
 
-	    for (int q = 0; q < two_class_classifier_sv_len; q++) {
-		coefs[model_id].push_back(sv_coefs_ptr[q]);
-	    }
-	    sv_coefs->releaseBlockOfRows(block_sv_coefs);
-	}
+        for (int q = 0; q < two_class_classifier_sv_len; q++) {
+        coefs[model_id].push_back(sv_coefs_ptr[q]);
+        }
+        sv_coefs->releaseBlockOfRows(block_sv_coefs);
+    }
     }
     Y_nt->releaseBlockOfRows(blockY);
 
@@ -184,7 +204,7 @@ construct_dual_coefs(
     permute_vector(coefs, perm);
 
     std::vector<std::vector<int> > sv_ind_by_class =
-	group_indices_by_class(n_classes, Y_data_ptr, sv_ind_by_clf);
+    group_indices_by_class(n_classes, Y_data_ptr, sv_ind_by_clf);
     auto mp = map_sv_to_columns_on_dual_coef_matrix(sv_ind_by_class);
 
     size_t num_unique_sv = mp.size();
@@ -192,19 +212,19 @@ construct_dual_coefs(
     std::vector<int> support_(num_unique_sv);
     int p = 0;
     for (int i=0; i < n_classes; i++) {
-	for(int j=i+1; j < n_classes; j++, p++) {
-	    std::vector<int> sv_ind_i_vs_j = sv_ind_by_clf[p];
-	    std::vector<double> sv_coef_i_vs_j = coefs[p];
+    for(int j=i+1; j < n_classes; j++, p++) {
+        std::vector<int> sv_ind_i_vs_j = sv_ind_by_clf[p];
+        std::vector<double> sv_coef_i_vs_j = coefs[p];
 
-	    int k = 0;
-	    for(auto sv_index : sv_ind_i_vs_j) {
-		int label = static_cast<int>(round(Y_data_ptr[sv_index]));
-		int col_index = mp[sv_index];
-		int row_index = (j == label) ? i : j - 1;
-		dual_coef_ptr[row_index*(num_unique_sv) + col_index] = sv_coef_i_vs_j[k];
-		support_[col_index] = sv_index;
-	    }
-	}
+        int k = 0;
+        for(auto sv_index : sv_ind_i_vs_j) {
+        int label = static_cast<int>(round(Y_data_ptr[sv_index]));
+        int col_index = mp[sv_index];
+        int row_index = (j == label) ? i : j - 1;
+        dual_coef_ptr[row_index*(num_unique_sv) + col_index] = sv_coef_i_vs_j[k];
+        support_[col_index] = sv_index;
+        }
+    }
     }
 
     return num_unique_sv;
@@ -212,21 +232,21 @@ construct_dual_coefs(
 
 template <typename dtype = double>
 std::tuple<da::classifier::training::ResultPtr, unsigned long>
-svm_fit(svm_linear_kernel_parameters svc_params, dm::NumericTablePtr Xt,
+svm_fit(svm_params svc_params, dm::NumericTablePtr Xt,
         dm::NumericTablePtr Yt, int n_classes, bool verbose) {
 
-    ds::SharedPtr<svm::training::Batch<dtype>>
-        training_algo_ptr(new svm::training::Batch<dtype>());
+    ds::SharedPtr<da::svm::training::Batch<dtype>>
+        training_algo_ptr(new da::svm::training::Batch<dtype>());
 
     /* Parameters for the SVM kernel function */
-    ds::SharedPtr<kernel_function::linear::Batch<dtype>>
-        kernel_ptr(new kernel_function::linear::Batch<dtype>());
+    ds::SharedPtr<da::kernel_function::linear::Batch<dtype>>
+        kernel_ptr(new da::kernel_function::linear::Batch<dtype>());
     size_t n_features = Xt->getNumberOfColumns();
     size_t n_samples = Xt->getNumberOfRows();
 
     training_algo_ptr->parameter.C = svc_params.C;
     training_algo_ptr->parameter.kernel = kernel_ptr;
-    training_algo_ptr->parameter.cacheSize = getOptimalCacheSize(n_samples);
+    training_algo_ptr->parameter.cacheSize = get_optimal_cache_size(n_samples);
     training_algo_ptr->parameter.accuracyThreshold = svc_params.tol;
     training_algo_ptr->parameter.tau = svc_params.tau;
     training_algo_ptr->parameter.maxIterations = svc_params.max_iter;
@@ -258,7 +278,7 @@ svm_fit(svm_linear_kernel_parameters svc_params, dm::NumericTablePtr Xt,
     algorithm->getInput()->set(da::classifier::training::labels, Yt);
 
     if (verbose) {
-        print_svm_linear_kernel_parameters(svc_params);
+        print_svm_params(svc_params);
     }
 
     /* Build the SVM model */
@@ -279,7 +299,7 @@ svm_fit(svm_linear_kernel_parameters svc_params, dm::NumericTablePtr Xt,
             = ds::dynamicPointerCast<da::svm::training::Result>(training_result);
         assert(svm_training_result);
         auto svm_model
-            = ds::dynamicPointerCast<svm::Model>(svm_training_result->get(classifier::training::model));
+            = ds::dynamicPointerCast<da::svm::Model>(svm_training_result->get(da::classifier::training::model));
         assert(svm_model);
         auto sv_idx = svm_model->getSupportIndices();
         sv_len = sv_idx->getNumberOfRows();
@@ -295,16 +315,16 @@ svm_fit(svm_linear_kernel_parameters svc_params, dm::NumericTablePtr Xt,
 
 
 template <typename dtype = double>
-dm::NumericTablePtr svm_predict(svm_linear_kernel_parameters svc_params,
+dm::NumericTablePtr svm_predict(svm_params svc_params,
                                 da::classifier::training::ResultPtr result,
                                 dm::NumericTablePtr X_nt, int n_classes,
                                 bool verbose) {
 
-    ds::SharedPtr<kernel_function::linear::Batch<dtype>>
-        kernel_ptr(new kernel_function::linear::Batch<dtype>());
+    ds::SharedPtr<da::kernel_function::linear::Batch<dtype>>
+        kernel_ptr(new da::kernel_function::linear::Batch<dtype>());
 
-    ds::SharedPtr<svm::prediction::Batch<dtype>>
-        two_class_pred_batch_algo_ptr(new svm::prediction::Batch<dtype>());
+    ds::SharedPtr<da::svm::prediction::Batch<dtype>>
+        two_class_pred_batch_algo_ptr(new da::svm::prediction::Batch<dtype>());
     two_class_pred_batch_algo_ptr->parameter.kernel = kernel_ptr;
 
     ds::SharedPtr<da::classifier::prediction::Batch> algorithm;
@@ -390,7 +410,7 @@ void bench(size_t threadNum, const std::string& X_fname,
         }
     }
 
-    svm_linear_kernel_parameters svm_problem;
+    svm_params svm_problem;
     svm_problem.C = 0.01;
     svm_problem.tol = 1e-16;
     svm_problem.tau = 1e-12;
@@ -418,8 +438,8 @@ void bench(size_t threadNum, const std::string& X_fname,
     for(int i=0; i < predict_samples; i++) {
         auto start = std::chrono::system_clock::now();
         for(int j=0; j < predict_repetitions; j++) {
-	    Yp_nt = svm_predict(svm_problem, training_result, X_nt, n_classes, verbose && (!i) && (!j));
-	}
+        Yp_nt = svm_predict(svm_problem, training_result, X_nt, n_classes, verbose && (!i) && (!j));
+    }
         auto finish = std::chrono::system_clock::now();
         predict_times.push_back(finish - start);
     }
@@ -427,25 +447,25 @@ void bench(size_t threadNum, const std::string& X_fname,
     double accuracy = accuracy_score(Y_nt, Yp_nt) * 100.00;
 
     if (header) {
-	std::cout << 
+    std::cout << 
         ""  << "prefix_ID"     <<
         "," << "function"      <<
-	    "," << "threads"       <<
-	    "," << "rows"          <<
-	    "," << "features"      <<
-	    "," << "cache-size-MB" <<
-	    "," << "fit"           <<
-	    "," << "predict"       <<
-	    "," << "accuracy"      <<
-	    "," << "sv-len"        <<
-	    "," << "classes"       << std::endl;
+        "," << "threads"       <<
+        "," << "rows"          <<
+        "," << "features"      <<
+        "," << "cache-size-MB" <<
+        "," << "fit"           <<
+        "," << "predict"       <<
+        "," << "accuracy"      <<
+        "," << "sv-len"        <<
+        "," << "classes"       << std::endl;
     }
 
     std::cout << "Native-C";
     std::cout << "," << "SVM";
     std::cout << "," << daal_thread_num;
     std::cout << "," << X_nt->getNumberOfRows() << "," << X_nt->getNumberOfColumns();
-    std::cout << "," << getOptimalCacheSize(X_nt->getNumberOfRows()) / (1024*1024);
+    std::cout << "," << get_optimal_cache_size(X_nt->getNumberOfRows()) / (1024*1024);
     std::cout << "," << std::min_element(fit_times.begin(), fit_times.end())->count() / fit_repetitions;
     std::cout << "," << std::min_element(predict_times.begin(), predict_times.end())->count() / predict_repetitions;
     std::cout << "," << accuracy;
