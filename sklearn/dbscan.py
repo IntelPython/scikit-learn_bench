22949e8ed8d3406691f22af8ee5969a8bf609352
Sameer Bibikar <sameer.bibikar@intel.com>
2020-02-14 12:41:13 -0600

    Only time DBSCAN.fit

--- sklearn/dbscan.py
+++ sklearn/dbscan.py
@@ -10,14 +10,14 @@ from sklearn.cluster import DBSCAN
 parser = argparse.ArgumentParser(description='scikit-learn DBSCAN benchmark')
 parser.add_argument('-x', '--filex', '--fileX', '--input', required=True,
                     type=str, help='Points to cluster')
-parser.add_argument('-e', '--eps', '--epsilon', type=float, default=0.5,
+parser.add_argument('-e', '--eps', '--epsilon', type=float, default=10,
                     help='Radius of neighborhood of a point')
 parser.add_argument('-m', '--data-multiplier', default=100,
                     type=int, help='Data multiplier')
 parser.add_argument('-M', '--min-samples', default=5, type=int,
                     help='The minimum number of samples required in a '
                     'neighborhood to consider a point a core point')
-params = parse_args(parser, loop_types=('fit', 'predict'), n_jobs_supported=True)
+params = parse_args(parser, n_jobs_supported=True)
 
 # Load generated data
 X = np.load(params.filex)
@@ -38,20 +38,12 @@ params.dtype = X.dtype
 print_header(columns, params)
 
 # Time fit
-fit_time, _ = time_mean_min(dbscan.fit, X,
-                            outer_loops=params.fit_outer_loops,
-                            inner_loops=params.fit_inner_loops,
-                            goal_outer_loops=params.fit_goal,
-                            time_limit=params.fit_time_limit,
-                            verbose=params.verbose)
-params.n_clusters = len(dbscan.core_sample_indices_)
-print_row(columns, params, function='DBSCAN.fit', time=fit_time)
-
-# Time predict
-predict_time, _ = time_mean_min(dbscan.fit_predict, X,
-                                outer_loops=params.predict_outer_loops,
-                                inner_loops=params.predict_inner_loops,
-                                goal_outer_loops=params.predict_goal,
-                                time_limit=params.predict_time_limit,
-                                verbose=params.verbose)
-print_row(columns, params, function='DBSCAN.fit_predict', time=predict_time)
+time, _ = time_mean_min(dbscan.fit, X,
+                        outer_loops=params.outer_loops,
+                        inner_loops=params.inner_loops,
+                        goal_outer_loops=params.goal,
+                        time_limit=params.time_limit,
+                        verbose=params.verbose)
+labels = dbscan.labels_
+params.n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+print_row(columns, params, function='DBSCAN', time=time)
