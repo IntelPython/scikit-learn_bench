fb87f6ec44934e11aa615a0566cbb29ee9f9897b
Alexander Andreev <alexander.andreev@intel.com>
2020-02-25 16:23:46 +0300

    change example config to unix format and runner modification

--- runner.py
+++ runner.py
@@ -14,6 +14,22 @@ from make_datasets import (
 )
 
 
+def verbose_print(text):
+    global verbose_mode
+    if verbose_mode:
+        print(text)
+
+
+def filter_stderr(text):
+    # delete 'Intel(R) DAAL usage in sklearn' messages
+    fake_error_message = 'Intel(R) Data Analytics Acceleration Library ' \
+                         + '(Intel(R) DAAL) solvers for sklearn enabled: ' \
+                         + 'https://intelpython.github.io/daal4py/sklearn.html'
+    while fake_error_message in text:
+        text = text.replace(fake_error_message, '')
+    return text
+
+
 def generate_cases(params):
     '''
     Generate cases for benchmarking by iterating of
@@ -29,13 +45,17 @@ def generate_cases(params):
     dashes = '-' if len(param_name) == 1 else '--'
     for i in range(n_param_values):
         for j in range(prev_length):
-            cases[prev_length * i + j] += f' {dashes}{param_name} {params[param_name][i]}'
+            cases[prev_length * i + j] += f' {dashes}{param_name} ' \
+                                          + f'{params[param_name][i]}'
     del params[param_name]
     generate_cases(params)
 
 
-read_stdout_from_command = lambda command: subprocess.run(
-    command.split(' '), stdout=subprocess.PIPE, encoding='utf-8').stdout[:-1]
+def read_output_from_command(command):
+    res = subprocess.run(command.split(' '), stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, encoding='utf-8')
+    return res.stdout[:-1], res.stderr[:-1]
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', metavar='ConfigPath',
@@ -44,9 +64,13 @@ parser.add_argument('--config', metavar='ConfigPath',
 parser.add_argument('--dummy-run', default=False, action='store_true',
                     help='Run configuration parser and datasets generation'
                          'without benchmarks running')
+parser.add_argument('--verbose', default=False, action='store_true',
+                    help='Print additional information during'
+                         'benchmarks running')
 parser.add_argument('--output-format', default='json', choices=('json', 'csv'),
                     help='Output type of benchmarks to use with their runner')
 args = parser.parse_args()
+verbose_mode = args.verbose
 
 with open(args.config.name, 'r') as config_file:
     config = json.load(config_file)
@@ -57,10 +81,10 @@ os.makedirs('data', exist_ok=True)
 csv_result = ''
 json_result = {'hardware': {}, 'results':[]}
 if 'Linux' in platform():
-    hostname = read_stdout_from_command('hostname')
-    batch = read_stdout_from_command('date -Iseconds')
+    hostname, _ = read_output_from_command('hostname')
+    batch, _ = read_output_from_command('date -Iseconds')
     # get CPU information
-    lscpu_info = read_stdout_from_command('lscpu')
+    lscpu_info, _ = read_output_from_command('lscpu')
     # remove excess spaces in CPU info output
     while '  ' in lscpu_info:
         lscpu_info = lscpu_info.replace('  ',' ')
@@ -80,7 +104,8 @@ for params_set in config['cases']:
     libs = params['lib']
     del params['dataset'], params['algorithm'], params['lib']
     generate_cases(params)
-    print(f'{algorithm} algorithm: {len(libs) * len(cases)} case(s), {len(params_set["dataset"])} dataset(s)\n')
+    verbose_print(f'{algorithm} algorithm: {len(libs) * len(cases)} case(s),' \
+                  + f' {len(params_set["dataset"])} dataset(s)\n')
     for dataset in params_set['dataset']:
         if dataset['training'].startswith('synth'):
             class GenerationArgs: pass
@@ -100,27 +125,31 @@ for params_set in config['cases']:
                 cls_num_for_file = '-' + dataset_params[4]
                 gen_args.classes = int(dataset_params[4])
                 gen_args.clusters = gen_args.classes
-                if gen_args.task == 'kmeans':
-                    gen_args.node_id = 0
-                    gen_args.filei = f'data/synth-{gen_args.task}-{gen_args.clusters}-init-{gen_args.samples}x{gen_args.features}.npy'
-                    paths += f'--filei {gen_args.filei}'
-                    gen_args.filet = f'data/synth-{gen_args.task}-{gen_args.clusters}-threshold-{gen_args.samples}x{gen_args.features}.npy'
             else:
                 cls_num_for_file = ''
 
-            gen_args.filex = f'data/synth-{gen_args.task}{cls_num_for_file}-X-train-{gen_args.samples}x{gen_args.features}.npy'
+            file_prefix = f'data/synth-{gen_args.task}{cls_num_for_file}-'
+            file_postfix = f'-{gen_args.samples}x{gen_args.features}.npy'
+
+            if gen_args.task == 'kmeans':
+                gen_args.node_id = 0
+                gen_args.filei = f'{file_prefix}init{file_postfix}'
+                paths += f'--filei {gen_args.filei}'
+                gen_args.filet = f'{file_prefix}threshold{file_postfix}'
+
+            gen_args.filex = f'{file_prefix}X-train{file_postfix}'
             paths += f' --file-X-train {gen_args.filex}'
             if gen_args.task not in ['kmeans', 'blobs']:
-                    gen_args.filey = f'data/synth-{gen_args.task}{cls_num_for_file}-y-train-{gen_args.samples}x{gen_args.features}.npy'
+                    gen_args.filey = f'{file_prefix}y-train{file_postfix}'
                     paths += f' --file-y-train {gen_args.filey}'
 
             if 'testing' in dataset.keys():
                 dataset_params = dataset['testing'].split('_')
                 _, _, gen_args.test_samples, _ = dataset_params[:4]
-                gen_args.filextest = f'data/synth-{gen_args.task}{cls_num_for_file}-X-test-{gen_args.test_samples}x{gen_args.features}.npy'
+                gen_args.filextest = f'{file_prefix}X-test{file_postfix}'
                 paths += f' --file-X-test {gen_args.filextest}'
                 if gen_args.task not in ['kmeans', 'blobs']:
-                    gen_args.fileytest = f'data/synth-{gen_args.task}{cls_num_for_file}-y-test-{gen_args.test_samples}x{gen_args.features}.npy'
+                    gen_args.fileytest = f'{file_prefix}y-test{file_postfix}'
                     paths += f' --file-y-test {gen_args.fileytest}'
             else:
                 gen_args.test_samples = 0
@@ -145,16 +174,16 @@ for params_set in config['cases']:
                 command = f'python {lib}/{algorithm}.py --batch {batch} --arch {hostname} --header --output-format {args.output_format}{case} {paths}'
                 while '  ' in command:
                     command = command.replace('  ', ' ')
-                print(command)
+                verbose_print(command)
                 if not args.dummy_run:
-                    r = subprocess.run(
-                        command.split(' '), stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE, encoding='utf-8')
+                    stdout, stderr = read_output_from_command(command)
+                    stderr = filter_stderr(stderr)
                     if args.output_format == 'json':
-                        json_result['results'] += json.loads(f'[{r.stdout}]')
+                        json_result['results'] += json.loads(f'[{stdout}]')
                     elif args.output_format == 'csv':
-                        csv_result += r.stdout + '\n'
-                    print(r.stderr, file=sys.stderr)
+                        csv_result += stdout + '\n'
+                    if stderr != '':
+                        print(stderr, file=sys.stderr)
 
 if args.output_format == 'json':
     json_result = json.dumps(json_result, indent=4)
