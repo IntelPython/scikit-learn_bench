b847226b1c942e642b0cb453ca59570db7d276ec
vlad-nazarov <vladislav.nazarov@intel.com>
2021-03-25 09:12:24 +0300

    Add device context parameter (#57)
    * Initial impl
    
    * Add another algs
    
    * Fix kmeans warning
    
    * Update config
    
    * Apply comments and fix pep
    
    * Fix device args
    
    * Fix pep8

--- sklearn_bench/dbscan.py
+++ sklearn_bench/dbscan.py
@@ -19,38 +19,43 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import bench
-from sklearn.metrics.cluster import davies_bouldin_score
 
-parser = argparse.ArgumentParser(description='scikit-learn DBSCAN benchmark')
-parser.add_argument('-e', '--eps', '--epsilon', type=float, default=10.,
-                    help='Radius of neighborhood of a point')
-parser.add_argument('-m', '--min-samples', default=5, type=int,
-                    help='The minimum number of samples required in a '
-                    'neighborhood to consider a point a core point')
-params = bench.parse_args(parser)
 
-from sklearn.cluster import DBSCAN
+def main():
+    from sklearn.cluster import DBSCAN
+    from sklearn.metrics.cluster import davies_bouldin_score
 
-# Load generated data
-X, _, _, _ = bench.load_data(params, add_dtype=True)
+    # Load generated data
+    X, _, _, _ = bench.load_data(params, add_dtype=True)
 
-# Create our clustering object
-dbscan = DBSCAN(eps=params.eps, n_jobs=params.n_jobs,
-                min_samples=params.min_samples, metric='euclidean',
-                algorithm='auto')
+    # Create our clustering object
+    dbscan = DBSCAN(eps=params.eps, n_jobs=params.n_jobs,
+                    min_samples=params.min_samples, metric='euclidean',
+                    algorithm='auto')
 
-# N.B. algorithm='auto' will select DAAL's brute force method when running
-# daal4py-patched scikit-learn, and probably 'kdtree' when running unpatched
-# scikit-learn.
+    # N.B. algorithm='auto' will select oneAPI Data Analytics Library (oneDAL)
+    # brute force method when running daal4py-patched scikit-learn, and probably
+    #  'kdtree' when running unpatched scikit-learn.
 
-# Time fit
-time, _ = bench.measure_function_time(dbscan.fit, X, params=params)
-labels = dbscan.labels_
+    # Time fit
+    time, _ = bench.measure_function_time(dbscan.fit, X, params=params)
+    labels = dbscan.labels_
 
-params.n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-acc = davies_bouldin_score(X, labels)
+    params.n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    acc = davies_bouldin_score(X, labels)
 
-bench.print_output(library='sklearn', algorithm='dbscan', stages=['training'],
-                   params=params, functions=['DBSCAN'], times=[time], accuracies=[acc],
-                   accuracy_type='davies_bouldin_score', data=[X],
-                   alg_instance=dbscan)
+    bench.print_output(library='sklearn', algorithm='dbscan', stages=['training'],
+                       params=params, functions=['DBSCAN'], times=[time],
+                       accuracies=[acc], accuracy_type='davies_bouldin_score',
+                       data=[X], alg_instance=dbscan)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='scikit-learn DBSCAN benchmark')
+    parser.add_argument('-e', '--eps', '--epsilon', type=float, default=10.,
+                        help='Radius of neighborhood of a point')
+    parser.add_argument('-m', '--min-samples', default=5, type=int,
+                        help='The minimum number of samples required in a '
+                        'neighborhood to consider a point a core point')
+    params = bench.parse_args(parser)
+    bench.run_with_context(params, main)
--- sklearn_bench/linear.py
+++ sklearn_bench/linear.py
@@ -21,36 +21,41 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import bench
 
-parser = argparse.ArgumentParser(description='scikit-learn linear regression '
-                                             'benchmark')
-parser.add_argument('--no-fit-intercept', dest='fit_intercept', default=True,
-                    action='store_false',
-                    help="Don't fit intercept (assume data already centered)")
-params = bench.parse_args(parser)
 
-from sklearn.linear_model import LinearRegression
+def main():
+    from sklearn.linear_model import LinearRegression
 
-# Load data
-X_train, X_test, y_train, y_test = bench.load_data(
-    params, generated_data=['X_train', 'y_train'])
+    # Load data
+    X_train, X_test, y_train, y_test = bench.load_data(
+        params, generated_data=['X_train', 'y_train'])
 
-# Create our regression object
-regr = LinearRegression(fit_intercept=params.fit_intercept,
-                        n_jobs=params.n_jobs, copy_X=False)
+    # Create our regression object
+    regr = LinearRegression(fit_intercept=params.fit_intercept,
+                            n_jobs=params.n_jobs, copy_X=False)
 
-# Time fit
-fit_time, _ = bench.measure_function_time(regr.fit, X_train, y_train, params=params)
+    # Time fit
+    fit_time, _ = bench.measure_function_time(regr.fit, X_train, y_train, params=params)
 
-# Time predict
-predict_time, yp = bench.measure_function_time(regr.predict, X_test, params=params)
+    # Time predict
+    predict_time, yp = bench.measure_function_time(regr.predict, X_test, params=params)
 
-test_rmse = bench.rmse_score(yp, y_test)
-yp = regr.predict(X_train)
-train_rmse = bench.rmse_score(yp, y_train)
+    test_rmse = bench.rmse_score(yp, y_test)
+    yp = regr.predict(X_train)
+    train_rmse = bench.rmse_score(yp, y_train)
 
-bench.print_output(library='sklearn', algorithm='linear_regression',
-                   stages=['training', 'prediction'],
-                   params=params, functions=['Linear.fit', 'Linear.predict'],
-                   times=[fit_time, predict_time], accuracy_type='rmse',
-                   accuracies=[train_rmse, test_rmse], data=[X_train, X_test],
-                   alg_instance=regr)
+    bench.print_output(library='sklearn', algorithm='linear_regression',
+                       stages=['training', 'prediction'],
+                       params=params, functions=['Linear.fit', 'Linear.predict'],
+                       times=[fit_time, predict_time], accuracy_type='rmse',
+                       accuracies=[train_rmse, test_rmse], data=[X_train, X_test],
+                       alg_instance=regr)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='scikit-learn linear regression '
+                                     'benchmark')
+    parser.add_argument('--no-fit-intercept', dest='fit_intercept', default=True,
+                        action='store_false',
+                        help="Don't fit intercept (assume data already centered)")
+    params = bench.parse_args(parser)
+    bench.run_with_context(params, main)
