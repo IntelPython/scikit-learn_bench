6a7efb56ba9153262694b5b47fcef1c59eb73ce0
Kirill Petrov <kirill.petrov@intel.com>
2021-04-16 02:34:48 +0300

    fix cuml

--- cuml_bench/svm.py
+++ cuml_bench/svm.py
@@ -48,7 +48,8 @@ params.cache_size_mb = cache_size_bytes / 1024**2
 params.n_classes = y_train[y_train.columns[0]].nunique()
 
 clf = SVC(C=params.C, kernel=params.kernel, cache_size=params.cache_size_mb,
-          tol=params.tol, gamma=params.gamma, random_state=43)
+          tol=params.tol, gamma=params.gamma,
+          probability=params.probability, random_state=43)
 
 fit_time, _ = bench.measure_function_time(clf.fit, X_train, y_train, params=params)
 params.sv_len = clf.support_.shape[0]
