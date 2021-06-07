503332859c35d3be1144fdcde7e0db262e3d4eda
Igor Rukhovich <igor.rukhovich@intel.com>
2021-06-07 10:35:08 +0300

    Second iteration of benchmark optimization (#72)
    * A huge modelbuilders benchmark renovation + fixing of a bug in a report generator + reporting metrics as well
    
    * preparing for merge
    
    * Applying mypy & pep8 suggestions
    
    * Added metric type as a separate field
    
    * Newlines at the end:)

--- modelbuilders_bench/xgb_mb.py
+++ modelbuilders_bench/xgb_mb.py
@@ -15,14 +15,24 @@
 # ===============================================================================
 
 import argparse
-import os
 
 import bench
 import daal4py
 import numpy as np
 import xgboost as xgb
 
-import modelbuilders_bench.mb_utils as utils
+
+def convert_probs_to_classes(y_prob):
+    return np.array([np.argmax(y_prob[i]) for i in range(y_prob.shape[0])])
+
+
+def convert_xgb_predictions(y_pred, objective):
+    if objective == 'multi:softprob':
+        y_pred = convert_probs_to_classes(y_pred)
+    elif objective == 'binary:logistic':
+        y_pred = y_pred.astype(np.int32)
+    return y_pred
+
 
 parser = argparse.ArgumentParser(
     description='xgboost gbt + model transform + daal predict benchmark')
@@ -36,6 +46,8 @@ parser.add_argument('--enable-experimental-json-serialization', default=True,
                     choices=('True', 'False'), help='Use JSON to store memory snapshots')
 parser.add_argument('--grow-policy', type=str, default='depthwise',
                     help='Controls a way new nodes are added to the tree')
+parser.add_argument('--inplace-predict', default=False, action='store_true',
+                    help='Perform inplace_predict instead of default')
 parser.add_argument('--learning-rate', '--eta', type=float, default=0.3,
                     help='Step size shrinkage used in update '
                          'to prevents overfitting')
@@ -106,71 +118,84 @@ xgb_params = {
 if params.threads != -1:
     xgb_params.update({'nthread': params.threads})
 
-if 'OMP_NUM_THREADS' in os.environ.keys():
-    xgb_params['nthread'] = int(os.environ['OMP_NUM_THREADS'])
-
 if params.objective.startswith('reg'):
     task = 'regression'
     metric_name, metric_func = 'rmse', bench.rmse_score
 else:
     task = 'classification'
-    metric_name, metric_func = 'accuracy[%]', utils.get_accuracy
+    metric_name = 'accuracy'
+    metric_func = bench.accuracy_score
     if 'cudf' in str(type(y_train)):
         params.n_classes = y_train[y_train.columns[0]].nunique()
     else:
         params.n_classes = len(np.unique(y_train))
+
+    # Covtype has one class more than there is in train
+    if params.dataset_name == 'covtype':
+        params.n_classes += 1
+
     if params.n_classes > 2:
         xgb_params['num_class'] = params.n_classes
 
 t_creat_train, dtrain = bench.measure_function_time(xgb.DMatrix, X_train,
                                                     params=params, label=y_train)
-t_creat_test, dtest = bench.measure_function_time(xgb.DMatrix, X_test, params=params)
+t_creat_test, dtest = bench.measure_function_time(
+    xgb.DMatrix, X_test, params=params, label=y_test)
 
 
-def fit(dmatrix=None):
+def fit(dmatrix):
     if dmatrix is None:
         dmatrix = xgb.DMatrix(X_train, y_train)
     return xgb.train(xgb_params, dmatrix, params.n_estimators)
 
 
-def predict():
-    dmatrix = xgb.DMatrix(X_test, y_test)
-    return model_xgb.predict(dmatrix)
+if params.inplace_predict:
+    def predict(*args):
+        return booster.inplace_predict(np.ascontiguousarray(X_test.values,
+                                                            dtype=np.float32))
+else:
+    def predict(dmatrix):  # type: ignore
+        if dmatrix is None:
+            dmatrix = xgb.DMatrix(X_test, y_test)
+        return booster.predict(dmatrix)
 
 
-t_train, model_xgb = bench.measure_function_time(
+fit_time, booster = bench.measure_function_time(
     fit, None if params.count_dmatrix else dtrain, params=params)
-train_metric = None
-if not X_train.equals(X_test):
-    y_train_pred = model_xgb.predict(dtrain)
-    train_metric = metric_func(y_train, y_train_pred)
+train_metric = metric_func(
+    convert_xgb_predictions(
+        booster.predict(dtrain),
+        params.objective),
+    y_train)
 
-t_xgb_pred, y_test_pred = bench.measure_function_time(predict, params=params)
-test_metric_xgb = metric_func(y_test, y_test_pred)
+predict_time, y_pred = bench.measure_function_time(
+    predict, None if params.inplace_predict or params.count_dmatrix else dtest, params=params)
+test_metric = metric_func(convert_xgb_predictions(y_pred, params.objective), y_test)
 
-t_trans, model_daal = bench.measure_function_time(
-    daal4py.get_gbt_model_from_xgboost, model_xgb, params=params)
+transform_time, model_daal = bench.measure_function_time(
+    daal4py.get_gbt_model_from_xgboost, booster, params=params)
 
 if hasattr(params, 'n_classes'):
     predict_algo = daal4py.gbt_classification_prediction(
         nClasses=params.n_classes, resultsToEvaluate='computeClassLabels', fptype='float')
-    t_daal_pred, daal_pred = bench.measure_function_time(
+    predict_time_daal, daal_pred = bench.measure_function_time(
         predict_algo.compute, X_test, model_daal, params=params)
     test_metric_daal = metric_func(y_test, daal_pred.prediction)
 else:
     predict_algo = daal4py.gbt_regression_prediction()
-    t_daal_pred, daal_pred = bench.measure_function_time(
+    predict_time_daal, daal_pred = bench.measure_function_time(
         predict_algo.compute, X_test, model_daal, params=params)
     test_metric_daal = metric_func(y_test, daal_pred.prediction)
 
-utils.print_output(library='modelbuilders',
-                   algorithm=f'xgboost_{task}_and_modelbuilder',
-                   stages=['xgboost_train', 'xgboost_predict', 'daal4py_predict'],
-                   params=params, functions=['xgb_dmatrix', 'xgb_dmatrix',
-                                             'xgb_train', 'xgb_predict',
-                                             'xgb_to_daal', 'daal_compute'],
-                   times=[t_creat_train, t_train, t_creat_test, t_xgb_pred,
-                          t_trans, t_daal_pred],
-                   accuracy_type=metric_name,
-                   accuracies=[train_metric, test_metric_xgb, test_metric_daal],
-                   data=[X_train, X_test, X_test])
+bench.print_output(
+    library='modelbuilders', algorithm=f'xgboost_{task}_and_modelbuilder',
+    stages=['training_preparation', 'training', 'prediction_preparation', 'prediction',
+            'transformation', 'alternative_prediction'],
+    params=params,
+    functions=['xgb.dmatrix.train', 'xgb.train', 'xgb.dmatrix.test', 'xgb.predict',
+               'daal4py.get_gbt_model_from_xgboost', 'daal4py.compute'],
+    times=[t_creat_train, fit_time, t_creat_test, predict_time, transform_time,
+           predict_time_daal],
+    accuracy_type=metric_name,
+    accuracies=[None, train_metric, None, test_metric, None, test_metric_daal],
+    data=[X_train, X_train, X_test, X_test, X_test, X_test])
