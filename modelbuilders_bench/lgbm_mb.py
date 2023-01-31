70344324dbbab02f66b6dd6093c918afed6bdea5
dmitry.razdoburdin <drazdobu@jflipp015.jf.intel.com>
2023-01-31 08:03:23 -0800

    fixing modelbuilder benchmark for covtype dataset

--- modelbuilders_bench/lgbm_mb.py
+++ modelbuilders_bench/lgbm_mb.py
@@ -99,7 +99,7 @@ else:
     if 'cudf' in str(type(y_train)):
         params.n_classes = y_train[y_train.columns[0]].nunique()
     else:
-        params.n_classes = len(np.unique(y_train))
+        params.n_classes = int(max(y_train)) + 1
     if params.n_classes > 2:
         lgbm_params['num_class'] = params.n_classes
 
