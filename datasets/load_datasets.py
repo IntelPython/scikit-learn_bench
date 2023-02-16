7ae38889ccce6d5c6214aac9cb6b569c4e84c535
Valerio Maggio <leriomaggio@users.noreply.github.com>
2023-02-16 19:47:47 +0000

    MyPy & PEP8 fix

--- datasets/load_datasets.py
+++ datasets/load_datasets.py
@@ -161,7 +161,7 @@ def extract_dataset_names(config_file: str) -> Set[str]:
     with open(config_file) as json_config_file:
         experiment = json.load(json_config_file)
 
-        if not "cases" in experiment:
+        if "cases" not in experiment:
             return set()
 
         datasets = list()
@@ -177,7 +177,8 @@ def extract_dataset_names(config_file: str) -> Set[str]:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Utility to download selected publicly available datasets included in the benchmark."
+        description="Utility to download selected publicly available datasets "
+        "included in the benchmark."
     )
     parser.add_argument(
         "-l",
@@ -214,7 +215,7 @@ if __name__ == "__main__":
     else:
         if args.configs:
             print(f"Dataset name(s) to download will be gathered from : {args.configs}")
-            ds_names = set()
+            ds_names: Set[str] = set()
             for config_file in args.configs:
                 ds_names = ds_names.union(extract_dataset_names(config_file))
         else:
