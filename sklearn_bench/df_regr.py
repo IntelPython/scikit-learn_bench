47275ea3e0958dd9a83f8262a7a6ee29f542a8b9
Ian Faust <icfaust@gmail.com>
2023-10-18 12:21:34 +0200

    Update df_regr.py

--- sklearn_bench/df_regr.py
+++ sklearn_bench/df_regr.py
@@ -37,7 +37,6 @@ def main():
                                  bootstrap=params.bootstrap,
                                  random_state=params.seed,
                                  n_jobs=params.n_jobs)
-    
     fit_time, _ = bench.measure_function_time(regr.fit, X_train, y_train, params=params)
 
     y_pred = regr.predict(X_train)
